3b42408f4aa4600a2f06b910ffda277c4f624bd6
Otto Hanski <otto.hanski@gmail.com>
2023-06-16 12:24:43 +0300

    Still not working

--- reyrey/raytracing/matrixcalc.py
+++ reyrey/raytracing/matrixcalc.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import pi
+from math import *
 import raytracing.matrices as ma
 
 def parseOSYS():
@@ -48,8 +48,24 @@ def transformq(ABCD, q1):
     q2 = (ABCD[0,0]*q1+ABCD[0,1])/(ABCD[1,0]*q1+ABCD[1,1])
     return q2
 
+
+### REPLACE EVERYTHING UNDER ###
+
+def w_z(z,lda,zr=None,w0=None,z0=0):
+    """calculates beam radius [m] based on z_r - rayleigh range [m] or waist radios w0 [m],lda - wavelength [m]"""
+    if zr == None:
+        zr = z_r(w0,lda)
+    try:
+        return sqrt(lda / pi * zr)*sqrt(1 + (z-z0)**2/zr**2)
+    except:
+        print(f"lda:{lda}\nzr: {zr}\nz: {z}")
+        print(zr)
+        print(lda)
+        
+        return 0
+
 class BeamTrace:
-    def __init__(self,matrexes,q_in,z0=0,n_points=1000):
+    def __init__(self,matrexes,q_in,z0=0, n_points=1000):
         self.n_points = n_points
         self.z0 = z0 #distance from q_in point to 0, needed only for convinience
         self.matrexes = matrexes # array of matrixes or labels - strings (names) at wich to calculate q-parameter
@@ -58,7 +74,7 @@ class BeamTrace:
         self.qz_to_print = [] # future array of (label,q) for labels in matrexes
         self.q_in = q_in # initial q-parameter of the beam
         
-    def constructRey(self):
+    def constructRey(self,lda = 972E-9):
         """Function that construct waists vs x posision"""
         self.xs = []
         self.ws = []
@@ -75,7 +91,7 @@ class BeamTrace:
                     
             if M[0][1] != 0: # it is free space matrix 
                 xs = np.linspace(0,M[0][1],self.n_points)
-                ws = w_z(xs+real(q_in),lda=lda,zr=imag(q_in)) # calculates waists
+                ws = w_z(xs+np.real(q_in),lda=lda,zr=np.imag(q_in)) # calculates waists
                 #print(ws)
                 
                 if self.xs != []: # point where if stoped in previous matrix
@@ -84,6 +100,8 @@ class BeamTrace:
                     extr = self.z0
                     
                 self.xs.extend(direction*xs+extr)
+                print(f"wsl: {self.ws}")
+                print(f"ws: {ws}")
                 self.ws.extend(ws)
                 
             elif M[0][1] == 0 and M[1][0] == 0: # ones matrix means a mirror - change of the direction
--- reyrey/reyrey.py
+++ reyrey/reyrey.py
@@ -1,7 +1,7 @@
 import raytracing.matrixcalc as rey
 import matplotlib.pyplot as plt
 
-testrey = rey.BeamTrace(rey.buildMatrixList(test = True), rey.calcq(Z = 0, ZR = 0, lam = 0, W = 0, n = 1))
+testrey = rey.BeamTrace(rey.buildMatrixList(test = True), rey.calcq(Z = 0, ZR = 10, lam = 0, W = 0, n = 1))
 testrey.constructRey()
 
 
